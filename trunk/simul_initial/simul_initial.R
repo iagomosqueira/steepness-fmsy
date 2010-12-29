9b951aed84a2a5b6c7bec4667767fd216c56d9e9
iago.mosqueira <iago.mosqueira@b44c09c0-d3b6-6530-7d79-dc58dc898670>
2010-12-29 17:15:25 +0000

    Adding papers

--- trunk/simul_initial/simul_initial.R
+++ trunk/simul_initial/simul_initial.R
@@ -43,15 +43,34 @@ plot(out$h, out$ratio, ylim=c(0,.5), type='b', pch=19)
 # Based on given s & v, create SR curve
 
 h <- 0.6
-res <- genBRP(age=1:25, Linf=100, k=exp(0.5235792+log(100)*-0.4540248),
-  a1=1, sL=0.5, sR=150, mat95=3, s=h, v=1e3)
 v <- 1e3
+res <- genBRP(age=1:25, Linf=100, k=exp(0.5235792+log(100)*-0.4540248),
+  a1=1, sL=0.5, sR=150, mat95=3, s=h, v=v)
 spr0 <- spr0(res)
 
-ssb <- seq(0.1, 5000, length=500)
+ssb <- FLQuant(seq(0.1, 5000, length=500))
 rec <- eval(as.list(bevholtSV()$model)[[3]], list(s=h, v=v, spr0=spr0, ssb=ssb))
 
 # Add variability with fixed CV at each SSB value, env. stochasticity
+resid <- FLQuant(exp(rnorm(99*100,0,.5)), dimnames=list(age=0,year=2:99,iter=1:100))
+
+# Better use vcov to generate correlated values of h & v
+# http://www.sitmo.com/doc/Generating_Correlated_Random_Numbers
+rho <- -0.89
+cv <- 0.3
+x <- rnorm(2000, h, h*cv)
+x <- rnorm(2000, 1, 2)
+x2 <- rnorm(2000, 1, 2)
+x <- x[x<1 & x > 0.2][1:1000]
+x2 <- rnorm(2000, v, v*cv)
+x2 <- x2[x2 > 0][1:1000]
+y <- rho*x + sqrt(1-(rho^2))*x2
+
+plot(x, x2, type='p', col='black', main=rho)
+points(x, y, col='red', pch=19)
+
+myDraws <- mvrnorm(1e5, mu=c(h, v), Sigma=matrix(c((h*cv)^2,
+  rho*sqrt((h*cv)^2*(v*cv)^2), rho*sqrt((h*cv)^2*(v*cv)^2), (v*cv)^2), 2, 2))
 
 # Estimate s & v
 
