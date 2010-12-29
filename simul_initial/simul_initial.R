9a1f2e079a789e17d2e8ee1ebee771253a38ad1e
iago.mosqueira <iago.mosqueira@b44c09c0-d3b6-6530-7d79-dc58dc898670>
2010-12-29 11:56:59 +0000

    Adding computing

--- simul_initial/simul_initial.R
+++ simul_initial/simul_initial.R
@@ -9,8 +9,26 @@
 
 # TODO Wed 29 Dec 2010 12:05:06 PM CET IM:
 
-# sim 1:
+# SIM 1:
 
 # Simulate population with h = seq(0.5, 0.95, by=0.05), sr$model='bevholt'
 
-# 
+# Ratio FMSY/Fcrash vs. h
+
+
+# SIM 2:
+
+# Add SR noise to FLBRP
+
+# Distribution of FMSY/Fcrash ratio for each value of h
+
+# Repeat for different levels of noise (CV)
+
+
+# SIM 3:
+
+# Create FLStock
+
+# fwd() @ FMSY_original w/ SR noise
+
+# FMSY/Fcrash
