86d6bfe9f983735863c42344e6701e82347cd72c
iago.mosqueira <iago.mosqueira@b44c09c0-d3b6-6530-7d79-dc58dc898670>
2010-12-29 11:19:57 +0000

    Adding simul_initial

---
+++ simul_initial/simul_initial.R
@@ -0,0 +1,16 @@
+# simul_initial - «Short one line description»
+# simul_initial
+
+# Copyright 2010 Iago Mosqueira, Cefas. Distributed under the GPL 2 or later
+# $Id:  $
+
+# Reference:
+# Notes:
+
+# TODO Wed 29 Dec 2010 12:05:06 PM CET IM:
+
+# sim 1:
+
+# Simulate population with h = seq(0.5, 0.95, by=0.05), sr$model='bevholt'
+
+# 
