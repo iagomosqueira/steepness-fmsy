712575e2dbbda5de4920cab8f8e01b9139637782
iago.mosqueira <iago.mosqueira@b44c09c0-d3b6-6530-7d79-dc58dc898670>
2010-12-29 14:46:01 +0000

    Adding computing

--- trunk/simul_initial/simul_initial.R
+++ trunk/simul_initial/simul_initial.R
@@ -9,16 +9,42 @@
 
 # TODO Wed 29 Dec 2010 12:05:06 PM CET IM:
 
+library(FLH)
+
 # SIM 1:
 
 # Simulate population with h = seq(0.5, 0.95, by=0.05), sr$model='bevholt'
 
+out <- data.frame(h=seq(0.5, 0.95, by=0.05), msy=NA, crash=NA)
+
+for (h in out$h) {
+
+  res <- genBRP(age=1:25, Linf=100, k=exp(0.5235792+log(100)*-0.4540248),
+    a1=1, sL=0.5, sR=150, mat95=3, s=h, v=1e3)
+
+  dimnames(refpts(res))$refpt[5]<-'crash'
+
+  res <- brp(res)
+
+  out$msy[out$h==h] <- refpts(res)['msy','harvest',]
+  out$crash[out$h==h] <- refpts(res)['crash','harvest',]
+}
+
+
 # Ratio FMSY/Fcrash vs. h
+
+out$ratio<-out$msy/out$crash
+
+plot(out$h, out$ratio, ylim=c(0,.5), type='b', pch=19)
 
 
 # SIM 2:
 
-# Add SR noise to FLBRP
+# Based on given s & v, create SR curve
+
+# Add variability with fixed CV at each SSB value, env. stochasticity
+
+# Estimate s & v
 
 # Distribution of FMSY/Fcrash ratio for each value of h
 
